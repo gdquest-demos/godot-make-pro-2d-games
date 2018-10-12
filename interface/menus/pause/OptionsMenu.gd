8178465d2baedb432538aeabd434ffa6be2836e8
Nathan Lovato <nathan@gdquest.com>
2018-10-12 14:07:54 +0900

    Make go back button close Options menu + queue_free, fix #55

--- interface/menus/pause/OptionsMenu.gd
+++ interface/menus/pause/OptionsMenu.gd
@@ -1,5 +1,12 @@
 extends "res://interface/menus/Menu.gd"
 
+func _ready():
+	open()
+
 func open():
 	.open()
-	$Column/MusicController/Row/HSlider.grab_focus()
+	$Column/GoBackButton.grab_focus()
+
+func close():
+	queue_free()
+	.close()
