934cfd1ec8b0dcaeae1ef311718a9164a219e6a7
Nathan Lovato <nathan@gdquest.com>
2018-10-12 15:59:59 +0900

    add focus support back to options menu
    I don't get why this issue happened in the first place, I've had to delete and re-append the scenes to the menu

--- interface/menus/pause/OptionsMenu.gd
+++ interface/menus/pause/OptionsMenu.gd
@@ -1,11 +1,8 @@
 extends "res://interface/menus/Menu.gd"
 
-func _ready():
-	open()
-
 func open():
 	.open()
-	$Column/GoBackButton.grab_focus()
+	$Column/MusicController/Row/HSlider.grab_focus()
 
 func close():
 	queue_free()
