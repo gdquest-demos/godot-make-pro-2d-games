588c715f14d201cf99d73830bcff33f471a5f863
Nathan Lovato <nathan@gdquest.com>
2018-10-12 16:59:00 +0900

    Make the door to get out of the cave inactive until you kill the boss

--- world/Door.gd
+++ world/Door.gd
@@ -5,10 +5,17 @@ signal player_entered(map_path)
 export(String, FILE, "*.tscn") var map_path
 var PlayerController = preload("res://actors/player/PlayerController.gd")
 
+export(bool) var ACTIVE_AT_START = true
+
 func _ready():
+	set_active(ACTIVE_AT_START)
 	assert map_path != ""
 
 func _on_body_entered(body):
 	if not body is PlayerController:
 		return
 	emit_signal("player_entered", map_path)
+
+func set_active(value):
+	visible = value
+	$CollisionShape2D.disabled = not value
