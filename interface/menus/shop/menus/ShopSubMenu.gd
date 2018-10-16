e603310130072a41730dc5e7840080b689afb17d
Nathan Lovato <nathan@gdquest.com>
2018-10-16 20:06:58 +0900

    Limit item buy amount based on the player's purse, fix #77

--- interface/menus/shop/menus/ShopSubMenu.gd
+++ interface/menus/shop/menus/ShopSubMenu.gd
@@ -22,7 +22,6 @@ func initialize(shop, buyer, items):
 		item_button.connect("pressed", info_panel, "_on_focused_Item_amount_changed", [item])
 	items_list.connect("focused_button_changed", self, "_on_ItemList_focused_button_changed")
 	items_list.initialize()
-	
 
 func _on_ItemList_focused_button_changed(item_button):
 	description_panel.display(item_button.description)
@@ -37,7 +36,11 @@ func close():
 
 func _on_ItemButton_pressed(shop, buyer, item):
 	var focused_item = get_focus_owner()
-	amount_popup.initialize(1, item.amount)
+	var price = shop.get_buy_value(item) if ACTION == "buy_from" else item.price
+	var coins = buyer.get_purse().coins
+	
+	var max_amount = floor(coins / price)
+	amount_popup.initialize(1, max_amount)
 	var amount = yield(amount_popup.open(), "completed")
 	focused_item.grab_focus()
 	if not amount:
