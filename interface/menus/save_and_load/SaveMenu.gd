2656d2b7c147ea2f4c5b4220d7218a0ff9d69f72
Nathan Lovato <nathan@gdquest.com>
2018-10-17 20:22:34 +0900

    Add Save and Load menu, second step for #62

---
+++ interface/menus/save_and_load/SaveMenu.gd
@@ -0,0 +1,8 @@
+extends "res://interface/menus/Menu.gd"
+
+onready var save_button = $Panel/Column/SaveButton
+onready var load_button = $Panel/Column/LoadButton
+
+func open(args):
+	.open()
+	save_button.grab_focus()
--- interface/menus/save_and_load/SaveUI.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func get_save_buttons():
-	return $Row/SaveButtons.get_children()
-
-func get_load_buttons():
-	return $Row/LoadButtons.get_children()
