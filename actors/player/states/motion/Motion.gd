5bf959867bddba90a04a24cf537fbf0866e87a84
Joshua Taylor <taylor.joshua88@gmail.com>
2018-11-05 22:22:02 -0600

    Renamed key_move_left etc to discrete_move_left

--- actors/player/states/motion/Motion.gd
+++ actors/player/states/motion/Motion.gd
@@ -8,10 +8,12 @@ func handle_input(event):
 func get_input_direction():
 	var input_direction = Vector2()
 	
-	input_direction.x = int(Input.is_action_pressed("key_move_right") or Input.is_action_pressed("joy_move_right")) - \
-		int(Input.is_action_pressed("key_move_left") or Input.is_action_pressed("joy_move_left"))
-	input_direction.y = int(Input.is_action_pressed("key_move_down") or Input.is_action_pressed("joy_move_down")) - \
-		int(Input.is_action_pressed("key_move_up") or Input.is_action_pressed("joy_move_up"))
+	input_direction.x = \
+		int(Input.is_action_pressed("discrete_move_right") or Input.is_action_pressed("joy_move_right")) - \
+		int(Input.is_action_pressed("discrete_move_left") or Input.is_action_pressed("joy_move_left"))
+	input_direction.y = \
+		int(Input.is_action_pressed("discrete_move_down") or Input.is_action_pressed("joy_move_down")) - \
+		int(Input.is_action_pressed("discrete_move_up") or Input.is_action_pressed("joy_move_up"))
 	
 	return input_direction
 
