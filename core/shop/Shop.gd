33b6b6980216c1ef8da38afc6195353be01bb775
Nathan Lovato <nathan@gdquest.com>
2018-11-18 13:29:01 +0900

    Ensure the shop has an inventory

--- core/shop/Shop.gd
+++ core/shop/Shop.gd
@@ -9,6 +9,9 @@ export(float, 0.0, 1.0) var BUY_MULTIPLIER = 0.25
 onready var inventory = $Inventory
 onready var purse = $Purse
 
+func _ready():
+	assert inventory != null
+
 func buy_from(actor, item, amount=1):
 	amount = clamp(amount, 1, MAX_TRANSACTION_COUNT)
 	var transaction_value = max(get_buy_value(item) * amount, purse.coins)
